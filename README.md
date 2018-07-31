8ed9f6212baa96f279af20d1bb24a78702267e8a
Mike <al-mn@ya.ru>
2018-07-31 20:35:57 +0300

    initial

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Selected links:
 + [Pass State as Props to Child Components](fcc-challenges/pass-state-as-props-to-child-components.js)
 
 + [Pass a Callback as Props](fcc-challenges/pass-a-callback-as-props.js)
+
++ [Manage Updates with Lifecycle Methods](fcc-challenges/manage-updates-with-lifecycle-methods.js)
+
++ [Optimize Re-Renders with shouldComponentUpdate](fcc-challenges/optimize-re-renders-with-shouldcomponentupdate.js)
---
+++ fcc-challenges/manage-updates-with-lifecycle-methods.js
@@ -0,0 +1,56 @@
+class Dialog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillUpdate() {
+    console.log('Component is about to update...');
+  }
+  // change code below this line
+  componentWillReceiveProps(nextProps){
+    console.log(this.props);
+    console.log(nextProps);
+  }
+
+  componentDidUpdate(){
+    console.log('Component updated');
+  }
+
+  // change code above this line
+  render() {
+    return <h1>{this.props.message}</h1>
+  }
+};
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'First Message'
+    };
+    this.changeMessage = this.changeMessage.bind(this);
+  }
+  changeMessage() {
+    this.setState({
+      message: 'Second Message'
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.changeMessage}>Update</button>
+        <Dialog message={this.state.message}/>
+      </div>
+    );
+  }
+};
+
+
+/* 
+Manage Updates with Lifecycle Methods
+
+The child component Dialog receives message props from its parent, the Controller component. Write the componentWillReceiveProps() method in the Dialog component and have it log this.props and nextProps to the console. You'll need to pass nextProps as an argument to this method and although it's possible to name it anything, name it nextProps here.
+
+Next, add componentDidUpdate() in the Dialog component, and log a statement that says the component has updated. This method works similar to componentWillUpdate(), which is provided for you. Now click the button to change the message and watch your browser console. The order of the console statements show the order the methods are called. 
+
+https://learn.freecodecamp.org/front-end-libraries/react/manage-updates-with-lifecycle-methods
+*/
