1fe131df3a78bf8162a53e572527cfa5ff024c70
Mike <al-mn@ya.ru>
2018-07-31 18:54:26 +0300

    add folder for fcc challenges

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # React-sandbox
+
 Playground for testing REACT functionality
+
+Selected links:
+
++ [Create a Controlled Form](fcc-challenges/controlled-form.js)
+
++ [Pass State as Props to Child Components](fcc-challenges/pass-state-as-props-to-child-components.js)
+
++ [Pass a Callback as Props](fcc-challenges/pass-a-callback-as-props.js)
---
+++ fcc-challenges/pass-state-as-props-to-child-components.js
@@ -0,0 +1,40 @@
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         <Navbar name = {this.state.name} />
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+}; 
+
+
+/* 
+
+Pass State as Props to Child Components
+
+The MyApp component is stateful and renders a Navbar component as a child. Pass the name property in its state down to the child component, then show the name in the h1 tag that's part of the Navbar render method.
+
+
+https://learn.freecodecamp.org/front-end-libraries/react/pass-state-as-props-to-child-components
+
+*/
